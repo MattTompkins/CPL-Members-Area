3e75baf871e9095029b13ab6b9c7b98188a4e5d4
MattTompkins <51715889+MattTompkins@users.noreply.github.com>
2023-06-10 19:00:57 +0100

    Create initial event routes

---
+++ app/Models/Event.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $table = 'events';
+
+    protected $fillable = [
+        'event_title',
+        'start_date',
+        'end_date',
+        'description',
+        'location',
+        'banner_image',
+        'show_on_website',
+        'members_only'
+    ];
+}
---
+++ database/migrations/2023_06_10_174541_create_events_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('event_title');
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
+            $table->longText('description');
+            $table->longText('location');
+            $table->longText('banner_image')->nullable();
+            $table->boolean('show_on_website')->default(FALSE);
+            $table->boolean('members_only')->default(FALSE);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+};
