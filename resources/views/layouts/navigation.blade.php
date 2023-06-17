42d67cbd0f62ecff947810df94e06ac2b940d5be
MattTompkins <51715889+MattTompkins@users.noreply.github.com>
2023-06-17 18:09:46 +0100

    Add events to main navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -19,71 +19,161 @@
         </div>
         <div class="hidden lg:flex lg:gap-x-12">
 
-              {{-- Dropdown menu start --}}
-       <div class="relative">
-        <button type="button" class="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-100"
-            aria-expanded="false" id="dropdownNavbarLink" data-dropdown-toggle="dropdownMenuExec">
-            {{ __('Exec') }}
-            <svg class="h-5 w-5 flex-none text-gray-400" viewBox="0 0 20 20" fill="currentColor"
-                aria-hidden="true">
-                <path fill-rule="evenodd"
-                    d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                    clip-rule="evenodd" />
-            </svg>
-        </button>
-        <div class="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl shadow-lg ring-1 ring-gray-900/5 bg-gray-900 shadow-2xl hidden"
-            id="dropdownMenuExec">
-            <div class="p-4">
+            {{-- Dropdown menu start --}}
+            <div class="relative">
+                <button type="button" class="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-100"
+                    aria-expanded="false" id="dropdownNavbarLink" data-dropdown-toggle="dropdownMenuExec">
+                    {{ __('Exec') }}
+                    <svg class="h-5 w-5 flex-none text-gray-400" viewBox="0 0 20 20" fill="currentColor"
+                        aria-hidden="true">
+                        <path fill-rule="evenodd"
+                            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                            clip-rule="evenodd" />
+                    </svg>
+                </button>
+                <div class="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl shadow-lg ring-1 ring-gray-900/5 bg-gray-900 shadow-2xl hidden"
+                    id="dropdownMenuExec">
+                    <div class="p-4">
 
-                {{-- Single dropdown item start --}}
-                <div
-                    class="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-700">
-                    <div
-                        class="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
-                          </svg>                          
-                    </div>
-                    <div class="flex-auto">
-                        <a href="{{ route('contacts.index') }}" class="block font-semibold text-gray-100">
-                            {{ __('Contacts') }}
-                            <span class="absolute inset-0"></span>
-                        </a>
-                        <p class="mt-1 text-gray-200">
-                            {{ __('Useful contacts for suppliers, clients or partner organisations.') }}</p>
-                    </div>
-                </div>
-                {{-- Single dropdown item end --}}
-
-                {{-- Single dropdown item start --}}
-                <div
-                    class="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-700">
-                    <div
-                        class="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
-                        </svg>
-
-                    </div>
-                    <div class="flex-auto">
-                        <a href="#" class="block font-semibold text-gray-100">
-                            {{ __('Maintence') }}
-                            <span class="absolute inset-0"></span>
-                        </a>
-                        <p class="mt-1 text-gray-200">
-                            {{ __('Submit, view or add maintence requests and reports.') }}</p>
+                        {{-- Single dropdown item start --}}
+                        <div
+                            class="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-700">
+                            <div
+                                class="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
+                                </svg>
+                            </div>
+                            <div class="flex-auto">
+                                <a href="{{ route('contacts.index') }}" class="block font-semibold text-gray-100">
+                                    {{ __('Contacts') }}
+                                    <span class="absolute inset-0"></span>
+                                </a>
+                                <p class="mt-1 text-gray-200">
+                                    {{ __('Useful contacts for suppliers, clients or partner organisations.') }}</p>
+                            </div>
+                        </div>
+                        {{-- Single dropdown item end --}}
                     </div>
                 </div>
-                {{-- Single dropdown item end --}}
-
-
             </div>
+            {{-- Dropdown menu end --}}
 
-        </div>
-    </div>
-    {{-- Dropdown menu end --}}
+            {{-- Dropdown menu start --}}
+            <div class="relative">
+                <button type="button" class="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-100"
+                    aria-expanded="false" id="dropdownNavbarLink" data-dropdown-toggle="dropdownMenuEvents">
+                    {{ __('Events') }}
+                    <svg class="h-5 w-5 flex-none text-gray-400" viewBox="0 0 20 20" fill="currentColor"
+                        aria-hidden="true">
+                        <path fill-rule="evenodd"
+                            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                            clip-rule="evenodd" />
+                    </svg>
+                </button>
+                <div class="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl shadow-lg ring-1 ring-gray-900/5 bg-gray-900 shadow-2xl hidden"
+                    id="dropdownMenuEvents">
+                    <div class="p-4">
+
+                        {{-- Single dropdown item start --}}
+                        <div
+                            class="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-700">
+                            <div
+                                class="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+                                </svg>
+
+                            </div>
+                            <div class="flex-auto">
+                                <a href="{{ route('events.index') }}" class="block font-semibold text-gray-100">
+                                    {{ __('Events list') }}
+                                    <span class="absolute inset-0"></span>
+                                </a>
+                                <p class="mt-1 text-gray-200">
+                                    {{ __('View a list of upcoming events to sign up for.') }}</p>
+                            </div>
+                        </div>
+                        {{-- Single dropdown item end --}}
+
+                        {{-- Single dropdown item start --}}
+                        <div
+                            class="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-700">
+                            <div
+                                class="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
+                                </svg>
+                            </div>
+                            <div class="flex-auto">
+                                <a href="{{ route('events.manage') }}" class="block font-semibold text-gray-100">
+                                    {{ __('Manage events') }}
+                                    <span class="absolute inset-0"></span>
+                                </a>
+                                <p class="mt-1 text-gray-200">
+                                    {{ __('View a list of current and past events to edit or delete.') }}</p>
+                            </div>
+                        </div>
+                        {{-- Single dropdown item end --}}
+
+                        {{-- Single dropdown item start --}}
+                        <div
+                            class="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-700">
+                            <div
+                                class="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M12 4.5v15m7.5-7.5h-15" />
+                                </svg>
+
+                            </div>
+                            <div class="flex-auto">
+                                <a href="{{ route('events.create') }}" class="block font-semibold text-gray-100">
+                                    {{ __('Create event') }}
+                                    <span class="absolute inset-0"></span>
+                                </a>
+                                <p class="mt-1 text-gray-200">
+                                    {{ __('Create a new upcoming event for members and/or the public to sign up to.') }}
+                                </p>
+                            </div>
+                        </div>
+                        {{-- Single dropdown item end --}}
+
+                        {{-- Single dropdown item start --}}
+                        <div
+                            class="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-700">
+                            <div
+                                class="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184" />
+                                </svg>
+                            </div>
+                            <div class="flex-auto">
+                                <a href="#" class="block font-semibold text-gray-100">
+                                    {{ __('Manage signups') }}
+                                    <span class="absolute inset-0"></span>
+                                </a>
+                                <p class="mt-1 text-gray-200">
+                                    {{ __('View, approve and reject signups for various events.') }}</p>
+                            </div>
+                        </div>
+                        {{-- Single dropdown item end --}}
+
+
+                    </div>
+
+                </div>
+            </div>
+            {{-- Dropdown menu end --}}
 
             {{-- Dropdown menu start --}}
             <div class="relative">
@@ -360,7 +450,7 @@
             </div>
             {{-- Dropdown menu end --}}
 
-         {{-- Dropdown menu start --}}
+            {{-- Dropdown menu start --}}
             <div class="relative">
                 <button type="button" class="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-100"
                     aria-expanded="false" id="dropdownNavbarLink" data-dropdown-toggle="dropdownMenuMembers">
@@ -448,9 +538,9 @@
 
                 </div>
             </div>
-    {{-- Dropdown menu end --}}
+            {{-- Dropdown menu end --}}
 
-     
+
 
 
         </div>
@@ -458,10 +548,10 @@
             <x-dropdown align="right" width="48">
                 <x-slot name="trigger">
                     <button
-                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-200 focus:outline-none transition ease-in-out duration-150">
-                        <div>{{ Auth::user()->name }}</div>
-
-                        <div class="ml-1">
+                        class="inline-flex items-center border border-transparent text-sm leading-4 font-medium rounded-md text-gray-200 focus:outline-none transition ease-in-out duration-150">
+                        <div><img class="h-10 w-10 flex-none rounded-full bg-gray-50"
+                                src="{{ Auth::user()->profile_image }}" /></div>
+                        <div class="ml-2">
                             <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                 <path fill-rule="evenodd"
                                     d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
@@ -472,6 +562,10 @@
                 </x-slot>
 
                 <x-slot name="content">
+                    <x-dropdown-link :href="route('profile.edit')">
+                        {{ __('My account') }}
+                    </x-dropdown-link>
+
                     <x-dropdown-link :href="route('profile.edit')">
                         {{ __('Profile') }}
                     </x-dropdown-link>
