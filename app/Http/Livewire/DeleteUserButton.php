e01313c5660243255a5af874a43c76dcefb394bd
MattTompkins <51715889+MattTompkins@users.noreply.github.com>
2023-06-17 13:48:32 +0100

    Delete user logic

---
+++ app/Http/Livewire/DeleteUserButton.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\User;
+
+class DeleteUserButton extends Component
+{
+    public $userId; // Add the userId property
+
+    protected $listeners = ['deleteConfirmed' => 'deleteMember'];
+
+    public function deleteConfirmation($id)
+    {
+        $this->userId = $id;
+        $this->dispatchBrowserEvent('launch-delete-modal');
+    }
+
+    public function deleteMember()
+    {
+        $member = User::findOrFail($this->userId);
+        $member->delete();
+        app('toast')->create( "This account has been successfully deleted.", 'success');
+        redirect()->route('members.index');
+    }
+
+    public function render()
+    {
+        return view('livewire.delete-user-button');
+    }
+}
